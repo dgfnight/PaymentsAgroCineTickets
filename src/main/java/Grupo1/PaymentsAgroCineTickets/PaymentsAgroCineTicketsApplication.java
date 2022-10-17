1545995ba8c77928adf0d2c6a6fbaefac467ed0a
derik <degaloni@gmail.com>
2022-10-17 20:01:55 -0300

    First Commit

---
+++ src/main/java/Grupo1/PaymentsAgroCineTickets/PaymentsAgroCineTicketsApplication.java
@@ -0,0 +1,13 @@
+package Grupo1.PaymentsAgroCineTickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsAgroCineTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsAgroCineTicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Grupo1/PaymentsAgroCineTickets/PaymentsAgroCineTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package Grupo1.PaymentsAgroCineTickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsAgroCineTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
